40a0a4ab763464d76d4ba3822a2ed20283be5825
Christoph Herzog <chris@theduke.at>
2026-05-03 17:54:56 +0200

    fix(wasix): Interrupt atomic waiters on signals
    Previously threads could get stuck idefinitely , even on KILL signals,
    because they would be stuck in atomic waiting.
    
    The downside of this implementation is that it can lead to spurious
    wakeups, but most data structures building on top of atomics can usually
    handle these!

--- lib/vm/src/instance/mod.rs
+++ lib/vm/src/instance/mod.rs
@@ -934,7 +934,7 @@ impl Instance {
             Err(_err) => {
                 // ret is None if there is more than 2^32 waiter in queue or some other error
                 // TODO: why THIS specific trap code tho? -.-
-                Err(Trap::lib(TrapCode::TableAccessOutOfBounds))
+                Err(Trap::lib(TrapCode::HostInterrupt))
             }
         }
     }
